f09e7ec26caec5fc51e36dc70c7f1beaf89a79eb
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2024-10-05 20:51:31 -0600

    post css config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+import purgecss from '@fullhuman/postcss-purgecss';
+
+export const plugins = [
+  'postcss-flexbugs-fixes',
+  'postcss-preset-env',
+  [
+    'tailwindcss',
+    'autoprefixer',
+  ],
+  purgecss({
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    safelist: ['html', 'body'], // Añade clases que no deben ser eliminadas
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  }),
+];
